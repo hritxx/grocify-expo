30d8e047f25178f5db99d4f475eaa9fce7d0b6a0
hritxx <hriteekroy1869@gmail.com>
2026-05-24 02:35:56 +0530

    feat(auth): integrate Clerk for authentication with sign-in and sign-up flows
    - Added ClerkProvider to the root layout for authentication management.
    - Implemented AuthRoutesLayout to handle redirection based on authentication state.
    - Created SignInScreen for user sign-in functionality.
    - Developed SignUp page with email verification and error handling.
    - Added home layout to restrict access based on authentication status.
    - Implemented user greeting and sign-out functionality on the home page.
    - Removed default index page and updated TypeScript configuration for nativewind.
    - Updated package dependencies for compatibility with new features.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,23 @@
 import { Stack } from "expo-router";
+import "../../global.css";
+
+import { ClerkProvider } from "@clerk/expo";
+import { tokenCache } from "@clerk/expo/token-cache";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error("Add your publishable key to the .env file");
+}
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <>
+      <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </ClerkProvider>
+    </>
+  );
 }
+
+
